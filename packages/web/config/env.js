d3b8d5bfebc893773db0836e78338fa8a8f11109
praveenantony12 <praveenantony12@yahoo.co.in>
2026-03-08 17:30:04 -0400

    Adding bot log reasoning in UI - enabled using ENABLE_BOT_REASONING_IN_UI env var

--- packages/web/config/env.js
+++ packages/web/config/env.js
@@ -56,13 +56,14 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
   .map(folder => path.resolve(appDirectory, folder))
   .join(path.delimiter);
 
-// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+// Grab NODE_ENV, REACT_APP_* and selected custom environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
 const REACT_APP = /^REACT_APP_/i;
+const CUSTOM_CLIENT_VARS = ['ENABLE_BOT_REASONING_IN_UI'];
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
-    .filter(key => REACT_APP.test(key))
+    .filter(key => REACT_APP.test(key) || CUSTOM_CLIENT_VARS.includes(key))
     .reduce(
       (env, key) => {
         env[key] = process.env[key];
